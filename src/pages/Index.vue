8e5a6e3ad05166c57acc9dac4f2c4f0a538b8392
Gerrishon Sirere <34297473+viewerdiscretion@users.noreply.github.com>
2021-01-13 12:02:46 +0300

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,8 +5,8 @@
       <nav>
         <!-- To use other icons here, you need to import them in the Shortcut component -->
         <Shortcut link="/getting-started" text="Introduction" icon="play-icon" />
-        <Shortcut link="/theme-configuration" text="Configuration" icon="sliders-icon" />
-        <Shortcut link="/theme-configuration#changing-colors" text="Change colors" icon="eye-icon" />
+        <Shortcut link="/basic-concepts" text="Usage" icon="sliders-icon" />
+        <Shortcut link="/basic-concepts#utilities" text="Change colors" icon="eye-icon" />
       </nav>
       <GitLink class="git" size="large" />
     </div>
